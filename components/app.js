605595639da30e6703906fa6b512b009273a31c5
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-21 20:12:16 +0000

    working minimal version of slider which avails of jQuery

---
+++ components/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by stephen on 21/03/15.
+ */
+'use strict';
+//var app = angular.module('sliderDemoApp', ['ui.slider', 'ngRoute']);
+var app = angular.module('sliderDemoApp', ['ui.slider', 'ngRoute'])
+    .config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/posts.html',
+            controller: 'PostsCtrl'
+        });
+    });
---
+++ components/services/colorPicker.js
@@ -0,0 +1,20 @@
+/**
+ * Created by stephen on 21/03/15.
+ */
+'use strict';
+
+app.factory('colorpicker', function () {
+    function hexFromRGB(r, g, b) {
+        var hex = [r.toString(16), g.toString(16), b.toString(16)];
+        angular.forEach(hex, function(value, key) {
+            if (value.length === 1) hex[key] = "0" + value;
+        });
+        return hex.join('').toUpperCase();
+    }
+    return {
+        refreshSwatch: function (r, g, b) {
+            var color = '#' + hexFromRGB(r, g, b);
+            angular.element('#swatch').css('background-color', color);
+        }
+    };
+});
