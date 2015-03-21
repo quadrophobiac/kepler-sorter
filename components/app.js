abac05271c6c692c10c442c8da3cc6ce1d80706a
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-21 22:37:08 +0000

    adding in resource as a dependency, necessary for async JSON requests

--- components/app.js
+++ components/app.js
@@ -2,7 +2,7 @@
  * Created by stephen on 21/03/15.
  */
 'use strict';
-var app = angular.module('sliderDemoApp', ['ngSlider']);
+var app = angular.module('sliderDemoApp', ['ngSlider', 'ngResource']);
 //var app = angular.module('sliderDemoApp', ['ngSlider', 'ngRoute'])
 //    .config(function ($routeProvider) {
 //    $routeProvider
--- components/services/KeplerAPI.js
+++ components/services/KeplerAPI.js
@@ -3,4 +3,4 @@
  */
 'use strict';
 
-app.factory('KeplerAPI', function(){});
+app.factory('KeplerAPI', function($q){});
