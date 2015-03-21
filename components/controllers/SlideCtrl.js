450d58903f95a8c7d71c72f48ee82806e0046234
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-21 20:51:15 +0000

    MVP implemented with minimal options

--- components/controllers/SlideCtrl.js
+++ components/controllers/SlideCtrl.js
@@ -7,7 +7,12 @@ app.controller('SlideCtrl', function($scope){
     $scope.value = "50";
     $scope.options = {
         from: 1,
-        to: 100,
-        step: 1
+        to: 9,
+        step: 1,
+        callback: function(value, released) {
+            // useful when combined with 'realtime' option
+            // released it triggered when mouse up
+            console.log(value + " " + released);
+        }
     };
 });
