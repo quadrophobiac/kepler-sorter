e3402207f811e58f289f1680badbcd5d22edef51
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-21 20:41:22 +0000

    suspending use of controllers while I stabilise the MVP

--- components/app.js
+++ components/app.js
@@ -2,12 +2,12 @@
  * Created by stephen on 21/03/15.
  */
 'use strict';
-
-var app = angular.module('sliderDemoApp', ['ngSlider', 'ngRoute'])
-    .config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/posts.html',
-            controller: 'PostsCtrl'
-        });
-    });
+var app = angular.module('sliderDemoApp', ['ngSlider']);
+//var app = angular.module('sliderDemoApp', ['ngSlider', 'ngRoute'])
+//    .config(function ($routeProvider) {
+//    $routeProvider
+//        .when('/', {
+//            templateUrl: 'views/posts.html',
+//            controller: 'PostsCtrl'
+//        });
+//    });
