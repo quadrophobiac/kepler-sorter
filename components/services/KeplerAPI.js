46fb731835596c054dd50c93cc40c2c6d855a86d
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-21 22:41:27 +0000

    creating a service

--- components/services/KeplerAPI.js
+++ components/services/KeplerAPI.js
@@ -3,4 +3,6 @@
  */
 'use strict';
 
-app.factory('KeplerAPI', function($q){});
+app.factory('KeplerAPI', function($q, $resource){
+    return $resource('http://www.asterank.com/api/kepler?query={%22PER%22:{%22$lt%22:1.02595675,%20%22$gt%22:0.67125}}&limit=10');
+});
