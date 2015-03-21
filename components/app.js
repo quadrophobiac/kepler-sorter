2d1891483f6255ff0b4d9df4f132982a63e18e58
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-21 20:39:05 +0000

    routing problems temporarily circumvented by using <base href=".."></base> - I know angular has a better way of doing this

--- components/app.js
+++ components/app.js
@@ -2,7 +2,7 @@
  * Created by stephen on 21/03/15.
  */
 'use strict';
-//var app = angular.module('sliderDemoApp', ['ui.slider', 'ngRoute']);
+
 var app = angular.module('sliderDemoApp', ['ngSlider', 'ngRoute'])
     .config(function ($routeProvider) {
     $routeProvider
