f80d0ff043d3cdc2706beaae423b611e403bcce6
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-22 21:28:19 +0000

    adding a server to this project

--- components/controllers/Request.js
+++ components/controllers/Request.js
@@ -1,3 +1,14 @@
 /**
  * Created by stephen on 22/03/15.
  */
+'use strict';
+
+app.controller('Request', function($scope, $http){
+    // functional URL = http://www.w3schools.com/website/Customers_JSON.php
+    $scope.test = "functional";
+    $http.get('http://www.w3schools.com/website/Customers_JSON.php').
+        success(function(data) {
+            $scope.greeting = data;
+        });
+
+});
