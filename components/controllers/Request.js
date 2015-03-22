85434b080599d90b042896b377eedd3f7dee9425
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-22 23:28:25 +0000

    have added in all the remedies suggested everywhere, no solution forthcoming

--- components/controllers/Request.js
+++ components/controllers/Request.js
@@ -3,12 +3,19 @@
  */
 'use strict';
 
-app.controller('Request', function($scope, $http){
+app.controller('Request', function($scope, $http, JSONP){
     // functional URL = http://www.w3schools.com/website/Customers_JSON.php
     $scope.test = "functional";
     $scope.get = function(){
 
-        $http.get('http://www.asterank.com/api/kepler?query={"PER":{"$lt":1.02595675,"$gt":0.67125}}&limit=10')
+        $http.get('http://www.asterank.com/api/kepler?query={"PER":{"$lt":1.02595675,"$gt":0.67125}}&limit=10',{
+            params: {
+                headers: {
+                    //'Access-Control-Allow-Origin': '*'
+                    'Access-Control-Request-Headers' : 'access-control-allow-origin'
+                }
+            }
+        })
             .success(function(result) {
                 console.log("Success", result);
                 $scope.result = result;
@@ -18,5 +25,8 @@ app.controller('Request', function($scope, $http){
         // the above is sending a GET request rather than an OPTIONS request
     };
 
-
+    $scope.grabJSON = function(){
+        $scope.data = JSONP.getResponse();
+        console.log($scope.data);
+    };
 });
--- nodeREST.js
+++ nodeREST.js
@@ -8,26 +8,12 @@ url = 'http://www.asterank.com/api/kepler?query={"PER":{"$lt":1.02595675,"$gt":0
 // get is a simple wrapper for request()
 // which sets the http method to GET
 var request = http.get(url, function (response) {
-    // data is streamed in chunks from the server
-    // so we have to handle the "data" event
-    var buffer = "",
-        data,
-        route;
-
+    var buffer = ""
     response.on("data", function (chunk) {
         buffer += chunk;
     });
-
     response.on("end", function (err) {
-        // finished transferring data
-        // dump the raw data
         console.log(buffer);
         console.log("\n");
-        data = JSON.parse(buffer);
-        route = data.routes[0];
-
-        // extract the distance and time
-        console.log("Walking Distance: " + route.legs[0].distance.text);
-        console.log("Time: " + route.legs[0].duration.text);
     });
 });
