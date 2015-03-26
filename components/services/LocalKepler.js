253e7a22eec7369f6f76fc6738b4c49775fecd5c
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-26 20:19:47 +0000

    skeleton of factory

--- components/services/LocalKepler.js
+++ components/services/LocalKepler.js
@@ -8,6 +8,18 @@ app.factory('LocalKepler', function($http){
 
     var KOI = {
 
+        get: function($http){
+            $http.get('mars-neptune.json')
+                .success(function(result){
+                    console.log("success");
+                    result = angular.fromJson(result);
+
+                })
+                .error(function(){
+                    console.log("error");
+                });
+        },
+
         KOI: {}
     };
 
